d86693d83f2a8ffd57c57f6bbe960414ce39a217
Juan Silva <58114554+SK0P3iN@users.noreply.github.com>
2021-05-20 17:00:07 +0100

    Initial Project Commit

---
+++ Assets/Agachamentos.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Agachamentos : MonoBehaviour
+{
+     public enum Direction
+    {
+        Stand = 0,
+        Left = 1,
+        Right,
+        Jump,
+        Count = 4
+    }
+
+    [Header("Main")]
+    [SerializeField] nuitrack.JointType targetJoint = nuitrack.JointType.LeftKnee;
+    [SerializeField] float deltaVelocity = 0.25f;
+
+    [SerializeField] nuitrack.JointType targetJointRightKnee = nuitrack.JointType.RightKnee;
+
+
+    const float scalK = 0.001f;
+    public GameObject Floor;
+    
+    float tamanhoPernas;
+    [Header("Test output")]
+    [SerializeField] Text text;
+    [SerializeField] Text text1;
+    [SerializeField] Text text2;
+    [SerializeField] Text Score;
+    [SerializeField] Text EstadoAtual;
+
+    public int score = 0;
+
+    public bool validarapenas1vezPerna = true;
+
+    public bool podeInstanciar1 = true, podeInstanciar2 = true;
+
+    public int apanharBola = 0;
+    public int naoapanharBola = 0;
+
+    public int LungeBemFeito, ContagemDeLunges;
+    public bool lungebool = true, lungebool2 = true;
+
+    public int lancamentosBowling = 0;
+
+    public bool ladoDireito = true, ladoEsquerdo = true;
+
+
+    public Transform LeftHand, RightHand;
+
+    public bool maodireitalancamento, maoesquerdalancamento;
+    public bool criarboladireita, criarbolaesquerda;
+
+    private Vector3 posicaoInicialLancamento;
+    public bool posicaoinicialLadoDireito, posicaoinicialLadoEsquerdo;
+
+    
+
+    public float forcadeLancamento = 20f;
+
+    public bool tem2lancamentos; //para mostrar que como tem 2 lancamentos para colocar bola para lancar no lado esquerdo
+    public float timer = 4f;
+    public bool TempoParaRestaurarPinos;
+    private List<Vector3> pinPositions;
+    private List<Quaternion> pinRotations;
+
+
+    private float timerTamanhoPernas = 5f;
+
+
+    // Public property
+    public Direction MoveDirection
+    {
+        get;
+        private set;
+    }
+
+    void Start()
+    {
+        /*var pins = GameObject.FindGameObjectsWithTag("Pin");
+        pinPositions = new List<Vector3>();
+        pinRotations = new List<Quaternion>();
+        foreach (var pin in pins)
+        {
+            pinPositions.Add(pin.transform.position);
+            pinRotations.Add(pin.transform.rotation);
+        }*/
+
+        NuitrackManager.onSkeletonTrackerUpdate += NuitrackManager_onSkeletonTrackerUpdate;
+    }
+
+    void NuitrackManager_onSkeletonTrackerUpdate(nuitrack.SkeletonData skeletonData)
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            var pins = GameObject.FindGameObjectsWithTag("Pin");
+
+            for(int i = 0; i < pins.Length; i++)
+            {
+                var pinPhysics = pins[i].GetComponent<Rigidbody>();
+                pinPhysics.velocity = Vector3.zero;
+                pinPhysics.position = pinPositions[i];
+                pinPhysics.rotation = pinRotations[i];
+                pinPhysics.velocity = Vector3.zero;
+                pinPhysics.angularVelocity = Vector3.zero;
+            }
+
+        }
+            
+        if(CurrentUserTracker.CurrentUser != 0)
+        {
+            nuitrack.Skeleton skeleton = CurrentUserTracker.CurrentSkeleton;
+
+            Vector3 leftKneePosition = scalK * skeleton.GetJoint(nuitrack.JointType.LeftKnee).ToVector3();
+
+            Vector3 rightKneePosition = scalK * skeleton.GetJoint(nuitrack.JointType.RightKnee).ToVector3();
+
+            float distSizeofLeg = Vector3.Distance(rightKneePosition, Floor.gameObject.transform.position);
+
+            float distJoelhoEsqaoChao = Vector3.Distance(Floor.gameObject.transform.position, leftKneePosition);
+
+            float distJoelhoDiraoChao = Vector3.Distance(Floor.gameObject.transform.position, rightKneePosition);
+
+
+
+
+
+            //Timer para guardar tamanho das pernas
+            if (validarapenas1vezPerna)
+            {
+                timerTamanhoPernas -= Time.deltaTime;
+                EstadoAtual.text = "Timer: " + timerTamanhoPernas;
+                if (timerTamanhoPernas <- 0)
+                {
+                    tamanhoPernas = distSizeofLeg;
+                    validarapenas1vezPerna = false;
+                    EstadoAtual.text = "Tamanho da perna: " + tamanhoPernas;
+                }
+                
+            }
+            
+            if(tamanhoPernas > 0)
+            {
+                //Lado Direito
+                Vector3 targetPostion = scalK * skeleton.GetJoint(targetJoint).ToVector3();
+                Vector3 moveDirection = targetPostion - rightKneePosition;
+
+                Vector3 velocityMove = Vector3.Project(rightKneePosition.normalized, moveDirection);
+
+
+                float distSeparacaodePernas = Vector3.Distance(rightKneePosition, targetPostion);
+
+                //Lado Esq
+                Vector3 targetPostionEsq = scalK * skeleton.GetJoint(targetJointRightKnee).ToVector3();
+                Vector3 moveDirectionEsq = targetPostionEsq - leftKneePosition;
+
+                Vector3 velocityMoveEsq = Vector3.Project(leftKneePosition, moveDirectionEsq);
+
+                float distSeparacaodePernasEsq = Vector3.Distance(leftKneePosition, targetPostionEsq);
+
+
+
+
+                //Debug.Log("Tamanho da perna: " + tamanhoPernas);
+                //Debug.Log("Separacao das pernas: " + distSeparacaodePernasEsq);
+                //Debug.Log("Distancia Joelho Direito ao chao:" + distJoelhoDiraoChao);
+                //Debug.Log("Distancia Joelho Esquerdo ao chao:" + distJoelhoEsqaoChao);
+
+
+                
+                //text.text = "" + distJoelhoDiraoChao;
+			}
+		}
+    }
+
+    public void ScoreIncrease()
+    {
+        score += 20;
+        Score.text = "Score: " + score;
+    }
+
+    public void CatchBall()
+    {
+        apanharBola++;
+    }
+
+    public void DidntCatchBall()
+    {
+        naoapanharBola++;
+    }
+
+}
---
+++ Assets/NuitrackSDK/Tutorials/First Project/NativeAvatar.cs
@@ -0,0 +1,131 @@
+﻿#region Description
+
+// The script performs a direct translation of the skeleton using only the position data of the joints.
+// Objects in the skeleton will be created when the scene starts.
+
+#endregion
+
+
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+[AddComponentMenu("Nuitrack/Example/TranslationAvatar")]
+public class NativeAvatar : MonoBehaviour
+{
+    string message = "";
+
+    public nuitrack.JointType[] typeJoint;
+    GameObject[] CreatedJoint;
+    public GameObject PrefabJoint;
+
+    const float scalK = 0.01f;
+    public bool validaPosicoes = true;
+    private float timerLePosicao = 5f;
+    public Text TimerPosicaoInicial;
+    public Text Agachamentos, Limite;
+
+    Vector3[] PosicoesIniciais = new Vector3[4];
+    public bool agachou = false;
+    public int agachamentoCounter = 0;
+
+    void Start()
+    {
+        CreatedJoint = new GameObject[typeJoint.Length];
+        for (int q = 0; q < typeJoint.Length; q++)
+        {
+            CreatedJoint[q] = Instantiate(PrefabJoint);
+            CreatedJoint[q].transform.SetParent(transform);
+        }
+        message = "Skeleton created";
+
+        TimerPosicaoInicial.text = "Posição Inicial";
+
+    }
+
+    void Update()
+    {
+        if (CurrentUserTracker.CurrentUser != 0)
+        {
+            nuitrack.Skeleton skeleton = CurrentUserTracker.CurrentSkeleton;
+            message = "Skeleton found";
+            
+            for (int q = 0; q < typeJoint.Length; q++)
+            {
+                nuitrack.Joint joint = skeleton.GetJoint(typeJoint[q]);
+                Vector3 newPosition = 0.001f * joint.ToVector3();
+                CreatedJoint[q].transform.localPosition = newPosition;
+            }
+
+            Vector3 OmbroDireitoPosition = scalK * skeleton.GetJoint(nuitrack.JointType.RightShoulder).ToVector3();
+            Vector3 OmbroEsquerdoPosition = scalK * skeleton.GetJoint(nuitrack.JointType.LeftShoulder).ToVector3();
+
+            Vector3 PescocoPosition = scalK * skeleton.GetJoint(nuitrack.JointType.Neck).ToVector3();
+            Vector3 CabecaPosition = scalK * skeleton.GetJoint(nuitrack.JointType.Head).ToVector3();
+
+            if (validaPosicoes)
+            {
+                timerLePosicao -= Time.deltaTime;
+                TimerPosicaoInicial.text = timerLePosicao.ToString();
+
+                if(timerLePosicao < 0)
+                {
+                    
+                    TimerPosicaoInicial.text = "Ta Lá";
+                    validaPosicoes = false;
+                    PosicoesIniciais[0] = OmbroDireitoPosition;
+                    PosicoesIniciais[1] = OmbroEsquerdoPosition;
+                    PosicoesIniciais[2] = PescocoPosition;
+                    PosicoesIniciais[3] = CabecaPosition;
+                }
+            }
+            else
+            {
+                float distAgachamentoOmbroDireito = PosicoesIniciais[0].y - OmbroDireitoPosition.y;/*Vector3.Distance(PosicoesIniciais[0], OmbroDireitoPosition);*/
+                float distAgachamentoOmbroEsquerdo = PosicoesIniciais[1].y - OmbroEsquerdoPosition.y;
+
+                float distAgachamentoPescoco = PosicoesIniciais[2].y - PescocoPosition.y;
+                float distAgachamentoCabeca = PosicoesIniciais[3].y - CabecaPosition.y;
+
+                if (!agachou)
+                {
+                    if ((distAgachamentoOmbroDireito > 3.0f) && (distAgachamentoOmbroEsquerdo > 3.0f) && (distAgachamentoPescoco > 2.0f) && (distAgachamentoCabeca > 2.0f))
+                    {
+                        agachou = true;
+                        Limite.text = agachou.ToString();
+                    }
+                        
+                }
+                else
+                {
+                    if ((distAgachamentoOmbroDireito < 0.5f) && (distAgachamentoOmbroEsquerdo < 0.5f) && (distAgachamentoPescoco < 0.5f) && (distAgachamentoCabeca < 0.5f))
+                    {
+                        agachou = false;
+                        Limite.text = agachou.ToString();
+                        agachamentoCounter++;
+                        print("TAI: " + agachamentoCounter);
+                        Agachamentos.text = agachamentoCounter.ToString();
+                    }
+                }
+            }
+
+            print("Direito: " + OmbroDireitoPosition);
+            print("Esquerdo: " + OmbroEsquerdoPosition);
+            print("Pescoco: " + PescocoPosition);
+            print("Cabeca: " + CabecaPosition);
+
+        }
+        else
+        {
+            message = "Skeleton not found";
+        }
+    }
+
+    // Display the message on the screen
+    void OnGUI()
+    {
+        GUI.color = Color.red;
+        GUI.skin.label.fontSize = 50;
+        GUILayout.Label(message);
+    }
+}
